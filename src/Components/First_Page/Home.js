cd1c52699c9378a31afb803a0e0ea0b178d1d753
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-06 03:36:44 +0000

    Minor changes made

--- src/Components/First_Page/Home.js
+++ src/Components/First_Page/Home.js
@@ -90,6 +90,145 @@ function Home() {
           </div>
         </div>
       </section>
+      <section id="features" className="features mt-5">
+        <div className="container">
+          {/* First Features Item */}
+          <div
+            className="row gy-4 py-5 align-items-center features-item"
+            data-aos="fade-up"
+          >
+            <div className="col-md-5">
+              <img
+                src="https://newsghana.com.gh/wp-content/uploads/2021/12/Social-%E2%80%93-Orphanage-donation--640x340.jpg"
+                className="img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-md-7">
+              <h3>
+                Voluptatem dignissimos provident quasi corporis voluptates sit
+                assumenda.
+              </h3>
+              <p className="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <ul>
+                <li>
+                  <i className="bi bi-check"></i> Ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </li>
+                <li>
+                  <i className="bi bi-check"></i> Duis aute irure dolor in
+                  reprehenderit in voluptate velit.
+                </li>
+                <li>
+                  <i className="bi bi-check"></i> Ullam est qui quos consequatur
+                  eos accusamus.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Second Features Item */}
+          <div
+            className="row gy-4 py-5 mt-5 align-items-center features-item"
+            data-aos="fade-up"
+          >
+            <div className="col-md-5 order-1 order-md-2">
+              <img
+                src="https://fefghana.org/wp-content/uploads/2016/05/Nyamadua-Donation-featured-image-780x437.jpeg"
+                className="img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-md-7 order-2 order-md-1">
+              <h3>Corporis temporibus maiores provident</h3>
+              <p className="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p>
+                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum
+              </p>
+            </div>
+          </div>
+
+          {/* Third Features Item */}
+          <div
+            className="row gy-4 py-5 mt-5 align-items-center features-item"
+            data-aos="fade-up"
+          >
+            <div className="col-md-5">
+              <img
+                src="https://www.globalgiving.org/pfil/14810/ph_14810_164205.jpg"
+                className="img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-md-7">
+              <h3>
+                Sunt consequatur ad ut est nulla consectetur reiciendis animi
+                voluptas
+              </h3>
+              <p>
+                Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus
+                quia minima quod. Sunt saepe odit aut quia voluptatem hic
+                voluptas dolor doloremque.
+              </p>
+              <ul>
+                <li>
+                  <i className="bi bi-check"></i> Ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </li>
+                <li>
+                  <i className="bi bi-check"></i> Duis aute irure dolor in
+                  reprehenderit in voluptate velit.
+                </li>
+                <li>
+                  <i className="bi bi-check"></i> Facilis ut et voluptatem
+                  aperiam. Autem soluta ad fugiat.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Fourth Features Item */}
+          <div
+            className="row gy-4 py-5 mt-5 align-items-center features-item"
+            data-aos="fade-up"
+          >
+            <div className="col-md-5 order-1 order-md-2">
+              <img
+                src="https://www.etharrelief.org/_D8A5990.png"
+                className="img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-md-7 order-2 order-md-1">
+              <h3>
+                Quas et necessitatibus eaque impedit ipsum animi consequatur
+                incidunt in
+              </h3>
+              <p className="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p>
+                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
