6fdcdb8238c54e65090feaf6cfef81e2f6730594
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-08 00:11:42 +0000

    minor change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/Nav_bar/NavBar";
 import Home from "./Components/First_Page/Home";
+import About from "./Components/About/About";
+import Gallery from "./Components/Gallery/Gallery";
+//import Mission from "./Components/Mission/Mission";
 import Footer from "./Components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
+import ContactUs from "./Components/Contact/ContactUs";
+import Donate from "./Components/Donate/Donate";
 
 function App() {
   return (
@@ -12,6 +17,13 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Gallery" element={<Gallery />} />
+        {/* 
+        <Route path="/Mission" element={<Mission />} />
+        */}
+        <Route path="Donate" element={<Donate />} />
+        <Route path="ContactUs" element={<ContactUs />} />
       </Routes>
       <Footer />
     </div>
