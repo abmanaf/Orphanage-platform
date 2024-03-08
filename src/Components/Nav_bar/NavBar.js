a8635296a8bfc4c4a73f3546b3fbf8c29bb88824
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-08 00:09:31 +0000

    minor changes

--- src/Components/Nav_bar/NavBar.js
+++ src/Components/Nav_bar/NavBar.js
@@ -16,93 +16,145 @@ function NavBar() {
 
   return (
     <div>
-      <div className="header">
+      <div className="main-header">
         <div>
-          <Link style={{ color: "white", textDecoration: "none" }} to="/">
-            <img src={Spread} alt="spread" width={80} height={50} />
-          </Link>
+          <h2>Welcome</h2>
         </div>
-        <div className={`side-bar ${isSideBarOpen ? "open" : "closed"}`}>
-          <div
-            className="close-icon"
-            onClick={closeSideBar}
-            style={{ float: "right" }}
-          >
-            <i className="fa fa-times" aria-hidden="true"></i>
+        <div className="header">
+          <div className="site-logo">
+            <Link style={{ color: "white", textDecoration: "none" }} to="/">
+              <img src={Spread} alt="spread" width={80} height={50} />
+            </Link>
           </div>
-          <br />
-          <br />
-          <Link
-            className="side-bar-context"
-            to="/"
-            closeSideBar={closeSideBar}
-            style={{ color: "black", textDecoration: "none" }}
-          >
-            About Us
-          </Link>
-          <br />
-          <br />
-
-          <Link
-            className="side-bar-context"
-            to="/"
-            closeSideBar={closeSideBar}
-            style={{ color: "black", textDecoration: "none" }}
-          >
-            Our Mission
-          </Link>
-          <br />
-          <br />
-
-          <Link
-            className="side-bar-context"
-            to="/"
-            closeSideBar={closeSideBar}
-            style={{ color: "black", textDecoration: "none" }}
-          >
-            Gallery
-          </Link>
-        </div>
-        <div>
-          <ul
-            style={{
-              display: "flex",
-              listStyleType: "none",
-              textDecoration: "none",
-            }}
-          >
-            <li>
-              <Link to="/">About Us</Link>
-            </li>
-            <li>
-              <Link to="/">Our Mission</Link>
-            </li>
-            <li>
-              <Link to="/">Gallery</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <div className="reach-us">
-            <ul>
+          <div className={`side-bar ${isSideBarOpen ? "open" : "closed"}`}>
+            <div
+              className="close-icon"
+              onClick={closeSideBar}
+              style={{ float: "right" }}
+            >
+              <i className="fa fa-times" aria-hidden="true"></i>
+            </div>
+            <br />
+            <br />
+            <Link
+              className="side-bar-context"
+              to="/About"
+              onClick={closeSideBar}
+              style={{
+                color: "black",
+                textDecoration: "none",
+                padding: "10px 10px",
+              }}
+            >
+              About Us
+            </Link>
+            <br />
+            <br />
+            {/* 
+            <Link
+              className="side-bar-context"
+              to="/Mission"
+              onClick={closeSideBar}
+              style={{
+                color: "black",
+                textDecoration: "none",
+                padding: "10px 10px",
+              }}
+            >
+              Our Mission
+            </Link>
+            <br />
+            <br />
+*/}
+            <Link
+              className="side-bar-context"
+              to="/Gallery"
+              onClick={closeSideBar}
+              style={{
+                color: "black",
+                textDecoration: "none",
+                padding: "10px 10px",
+              }}
+            >
+              Gallery
+            </Link>
+            <br />
+            <br />
+            <Link
+              className="side-bar-context"
+              to="/Donate"
+              onClick={closeSideBar}
+              style={{
+                color: "black",
+                textDecoration: "none",
+                padding: "10px 10px",
+              }}
+            >
+              Donate
+            </Link>
+            <br />
+            <br />
+            <div className="reach-us" style={{}}>
+              <Link
+                onClick={closeSideBar}
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  backgroundColor: "#c35017",
+                  padding: "10px 10px",
+                  borderRadius: "10px",
+                }}
+                to="/ContactUs"
+              >
+                Reach Us
+              </Link>
+            </div>
+          </div>
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                listStyleType: "none",
+                textDecoration: "none",
+              }}
+            >
               <li>
-                <Link style={{ color: "white" }} to="/">
-                  Reach Us
-                </Link>
+                <Link to="/About">About Us</Link>
+              </li>
+              {/*
+              <li>
+                <Link to="/Mission">Our Mission</Link>
+              </li>
+               */}
+              <li>
+                <Link to="/Gallery">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/Donate">Donate</Link>
               </li>
             </ul>
           </div>
-          <div
-            className="menu-icon"
-            style={{ fontSize: "2em", marginTop: "0" }}
-            onClick={openSideBar}
-          >
-            <Link to="/">
-              <i className="fa fa-bars" aria-hidden="true"></i>
-            </Link>
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <div className="reach-us">
+              <ul>
+                <li>
+                  <Link style={{ color: "white" }} to="/ContactUs">
+                    Reach Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div
+              className="menu-icon"
+              style={{ fontSize: "2em", marginTop: "0" }}
+              onClick={openSideBar}
+            >
+              <Link to="#" style={{ textDecoration: "none" }}>
+                <i className="fa fa-bars" aria-hidden="true"></i>
+              </Link>
+            </div>
           </div>
-        </div>
-        {/* 
+          {/* 
         <div className="menu-button">
           <i
             style={{ fontSize: "2em", marginTop: "0" }}
@@ -111,6 +163,7 @@ function NavBar() {
           ></i>
         </div>
         */}
+        </div>
       </div>
     </div>
   );
