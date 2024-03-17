1592b49e8d60daa59cce2a0cab12909f0e087f7b
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-17 11:16:39 +0000

    minor change

--- src/Components/Nav_bar/NavBar.js
+++ src/Components/Nav_bar/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Spread from "./spread_love.jpg";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+//import "font-awesome/css/font-awesome.min.css";
 
 function NavBar() {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
